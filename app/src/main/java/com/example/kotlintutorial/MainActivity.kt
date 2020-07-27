24c34e0628ff2c64cff52dc12c9ebc062e26c09e
psavvaidis <panagiotissav@gmail.com>
2020-07-27 22:01:35 +0300

    Update onClick functionality Show times clicked

--- app/src/main/java/com/example/kotlintutorial/MainActivity.kt
+++ app/src/main/java/com/example/kotlintutorial/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.kotlintutorial
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -11,9 +12,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btn_click_me = findViewById(R.id.button) as Button
+        val btn_click_me = findViewById<Button>(R.id.button) as Button
+        val myTextView = findViewById<TextView>(R.id.textView) as TextView
+        var timesClicked = 0
 
         btn_click_me.setOnClickListener{
+            timesClicked += 1
+            myTextView.text = timesClicked.toString()
             Toast.makeText(this@MainActivity, "You clicked me", Toast.LENGTH_SHORT).show()
         }
     }
